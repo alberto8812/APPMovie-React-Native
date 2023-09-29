377b1fdc29a6f627f581e65a1a4087383c328b14
alberto8812 <cvelascosaavedra@gmail.com>
2023-09-28 21:55:13 -0500

    install expo

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ActivityIndicator, Dimensions, ScrollView, View } from 'react-native'
 import movieDB from '../api/movieDB'
 import { useMovies } from '../hooks/useMovies'
 import { GradientBackground, HorizontalSlider, MovieCard } from '../components'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Carousel from 'react-native-snap-carousel';
+import ImageColors, {getColors} from 'react-native-image-colors'
 
 const {width:windowWidth}=Dimensions.get('window')
 
 export const HomeScreen = () => {
 
- const {nowPlaying,IsLoading,popular,topRated,upComing} =useMovies();
+const {nowPlaying,IsLoading,popular,topRated,upComing} =useMovies();
 const {top}=useSafeAreaInsets();
-  
+
+
+const getPosterColors=async(index:number)=>{
+
+  const Movie=nowPlaying[index];
+  const uri=`https://image.tmdb.org/t/p/w500${Movie.poster_path}`;
+  const colors=await ImageColors.getColors(uri, {})
+  console.log(colors)
+}
+
+
  if(IsLoading) {
   return(<View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
     <ActivityIndicator color="blue" size={100}/>
@@ -33,6 +44,7 @@ const {top}=useSafeAreaInsets();
       sliderWidth={windowWidth}
       itemWidth={300}
       inactiveSlideOpacity={0.9}
+      onSnapToItem={index=>getPosterColors(index)}
       />
      </View >
 
